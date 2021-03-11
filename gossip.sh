7a87755a01cbf678da59b3dc86635e3d8ac4108c
ArjunKishore <arjunitp@gmail.com>
2021-03-11 23:40:56 +0800

    fix shell script errors

--- gossip.sh
+++ gossip.sh
@@ -4,21 +4,23 @@ if [ "$PEER" == "" ]; then
     killall php
 else
     echo "Bootstrapping network with node $PEER"
+    # shellcheck disable=SC2006
+    peerPort=`cat data/"$PEER".port`
 fi
-rm -rf data/$USER.json
+rm -rf data/"$USER".json
 port=8000
 retry=30
 while [ $retry -gt 0 ]
 do
-    if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null ;then
-       let retry -=1
-       let port +=1
+    if lsof -Pi :$port -sTCP:LISTEN -t >/dev/null ; then
+           (( retry -=1 ))
+           (( port +=1 ))
     else
         break
     fi
 done
 
-echo $port > data/$USER.port
-php -S 127.0.0.1:$port &
+echo "$port" > data/"$USER".port
+php -S 127.0.0.1:"$port" &
 echo ""
-php gossip.php $port $peerPort
+php gossip.php "$port" "$peerPort"
